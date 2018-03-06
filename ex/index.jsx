79ea9983e2c9c1b5aee8f030d04b10dce72ab229
SkyList <matheus.pantoja97@gmail.com>
2018-03-06 10:52:45 -0300

    componentes filhos

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Primeiro, Segundo } from './componente'
+import SilvaFamily from './silvaFamily'
 
 ReactDOM.render(
-    <div>
-        <Primeiro/>
-        <Segundo/>
-    </div>
+    <SilvaFamily/>
     ,document.getElementById('app')
 )
 
