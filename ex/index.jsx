76628c7cc084aca3f7a65dd7a20b44cc61c869d1
SkyList <matheus.pantoja97@gmail.com>
2018-03-06 11:00:43 -0300

    componentes filhos 2

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Primeiro, Segundo } from './componente'
 import SilvaFamily from './silvaFamily'
+import Family from './family'
+import Member from './member'
 
 ReactDOM.render(
-    <SilvaFamily/>
+        <Family>
+            <h1>Familia</h1>
+            <Member name='Matheus' lastName='Pantoja'/>
+        </Family>
     ,document.getElementById('app')
 )
 
